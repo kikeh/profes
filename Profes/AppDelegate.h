e16626b29839837145c53f558a85e20a037f5ad4
Kike Isidoro <Kike@iKike.local>
2012-03-29 17:45:15 +0200

    Initial Commit

---
+++ Profes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Profes
+//
+//  Created by Kike Isidoro on 29/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Profes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Profes
+//
+//  Created by Kike Isidoro on 29/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
